b57e147afdabfde7e753c066368d2682635b3164
mthtitumir <titumirgccc@gmail.com>
2023-03-28 01:32:08 +0600

    eventHandler added

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -3,23 +3,34 @@ import Product from '../Product/Product';
 import './Shop.css'
 
 const Shop = () => {
-    const [products, setProducts] = useState([]); 
-    useEffect(()=>{
+    const [products, setProducts] = useState([]);
+    const [cart, setCart] = useState([]);
+    useEffect(() => {
         fetch('products.json')
-        .then(res => res.json())
-        .then(data => setProducts(data))
+            .then(res => res.json())
+            .then(data => setProducts(data))
     }, []);
+    const handleAddToCart = (product) => {
+        // cart.push(product) vanilla js method
+        const newCart = [...cart, product];
+        setCart(newCart);
+    }
     return (
         <div className='shop-container'>
             <div className="products-container">
                 {
-                    products.map(product => <Product key={product.id} product={product}></Product>)
+                    products.map(product => <Product
+                        key={product.id}
+                        product={product}
+                        handleAddToCart={handleAddToCart}
+                    ></Product>)
                 }
             </div>
             <div className="cart-container">
                 <h4>Order Summary</h4>
+                <p>Selected Items: {cart.length}</p>
             </div>
-            
+
         </div>
     );
 };
